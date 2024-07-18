34674582a7b8d00c898464108aa9d80174879839
Ted Perez <taperez1989@yahoo.com>
2024-07-17 19:28:23 -0700

    created models

--- models/index.js
+++ models/index.js
@@ -2,12 +2,16 @@ const User = require("./user");
 const Games = require("./games");
 
 
-User.hasMany(Games, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-Games.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+Title.belongsTo(Genre, { foreignKey: 'genreId' });
+Title.belongsToMany(Platform, { through: TitlesPlatforms, foreignKey: 'titleId' });
+Platform.belongsToMany(Title, { through: TitlesPlatforms, foreignKey: 'platformId' });
+
+// User.hasMany(Games, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
+// Games.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
 module.exports = { User, Games };
