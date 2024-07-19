3881f89759400f46619cfbc1ee3b95c74b9e3f07
Ted Perez <taperez1989@yahoo.com>
2024-07-18 18:43:45 -0700

    code mods

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('../controllers/api');
 
-const router = require("express").Router();
-const apiRoutes = require("./api");
-
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
