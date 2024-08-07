7548787dfe195f84b32eef34e79ce93a4c2cb1ff
AcoderRose <addyro96@att.net>
2024-08-06 19:22:21 -0700

    Made changes to a lot of files. Debugged login functionality and image render size.

--- public/js/login.js
+++ public/js/login.js
@@ -1,46 +1,47 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
   if (email && password) {
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
+    const response = await fetch("/api/user/login", {
+      method: "POST",
       body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
+      document.location.replace("/");
       const data = await response.json(); // Parse the JSON from the response
-        console.log('response data', data); // Log the response data
-      localStorage.setItem('response', JSON.stringify(response));
-      document.location.replace('/');
-      console.log('response', response);
+      console.log("response data", data); // Log the response data
+      localStorage.setItem("response", JSON.stringify(response));
+      // document.location.replace('/');
+      console.log("response", response);
     } else {
-      alert('Failed to log in.');
+      alert("Failed to log in.");
     }
   }
 };
 const signupFormHandler = async (event) => {
   event.preventDefault();
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
   if (username && email && password) {
-    const response = await fetch('/api/user', {
-      method: 'POST',
+    const response = await fetch("/api/user", {
+      method: "POST",
       body: JSON.stringify({ username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
-      console.log('response', response);
-      document.location.replace('/user');
+      console.log("response", response);
+      document.location.replace("/user");
     } else {
-      alert('Failed to sign up.');
+      alert("Failed to sign up.");
     }
   }
 };
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
