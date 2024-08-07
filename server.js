14a6b41218a77192d3cab6d19387f3fb900e2ff6
haartmuhn <164945655+haartmuhn@users.noreply.github.com>
2024-08-07 16:33:24 -0700

    Update server.js
    Adjustment needed for RENDER deployment

--- server.js
+++ server.js
@@ -180,7 +180,6 @@ app.get("/", async (req, res) => {
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () =>
-    console.log(`Server is listening on http://localhost:${PORT}`)
-  );
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
+;
 });
