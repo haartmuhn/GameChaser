6a28f7876946100c8333bde1dcd81209a6554d26
haartmuhn <164945655+haartmuhn@users.noreply.github.com>
2024-08-07 16:47:04 -0700

    Update connection.js
    Reconfigured for RENDER

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-const connection = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "postgres",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
-
-module.exports = connection;
+module.exports = sequelize;
